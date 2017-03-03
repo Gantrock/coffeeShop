6b2eaf0259d88f859832e7290316b7b57da58341
{nickveilleux} <{veilleux@uw.edu}>
2017-03-03 15:07:47 -0800

    added review class

---
+++ src/main/java/objects/Review.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objects;
+
+import java.util.Date;
+
+/**
+ *
+ * @author nick
+ */
+public class Review {
+    private String myContent;
+    private Date myDate;
+    private int shopid;
+    private int myWifi;
+    private int myCoffee;
+    private int myFood;
+    private int myStudy;
+
+    /**
+     * @return the myContent
+     */
+    public String getMyContent() {
+        return myContent;
+    }
+
+    /**
+     * @param myContent the myContent to set
+     */
+    public void setMyContent(String myContent) {
+        this.myContent = myContent;
+    }
+
+    /**
+     * @return the myDate
+     */
+    public Date getMyDate() {
+        return myDate;
+    }
+
+    /**
+     * @param myDate the myDate to set
+     */
+    public void setMyDate(Date myDate) {
+        this.myDate = myDate;
+    }
+
+    /**
+     * @return the shopid
+     */
+    public int getShopid() {
+        return shopid;
+    }
+
+    /**
+     * @param shopid the shopid to set
+     */
+    public void setShopid(int shopid) {
+        this.shopid = shopid;
+    }
+
+    /**
+     * @return the myWifi
+     */
+    public int getMyWifi() {
+        return myWifi;
+    }
+
+    /**
+     * @param myWifi the myWifi to set
+     */
+    public void setMyWifi(int myWifi) {
+        this.myWifi = myWifi;
+    }
+
+    /**
+     * @return the myCoffee
+     */
+    public int getMyCoffee() {
+        return myCoffee;
+    }
+
+    /**
+     * @param myCoffee the myCoffee to set
+     */
+    public void setMyCoffee(int myCoffee) {
+        this.myCoffee = myCoffee;
+    }
+
+    /**
+     * @return the myFood
+     */
+    public int getMyFood() {
+        return myFood;
+    }
+
+    /**
+     * @param myFood the myFood to set
+     */
+    public void setMyFood(int myFood) {
+        this.myFood = myFood;
+    }
+
+    /**
+     * @return the myStudy
+     */
+    public int getMyStudy() {
+        return myStudy;
+    }
+
+    /**
+     * @param myStudy the myStudy to set
+     */
+    public void setMyStudy(int myStudy) {
+        this.myStudy = myStudy;
+    }
+   
+
+
+
+
+
+
+}
