39e8cb37d1bfe59ed931bccf9bdb04cf3d7e98c9
{nickveilleux} <{veilleux@uw.edu}>
2017-03-04 21:12:41 -0800

    something

--- src/scripts/create_review.sh
+++ src/scripts/create_review.sh
@@ -4,7 +4,7 @@ ENDPOINT=https://sheltered-earth-81944.herokuapp.com/tcss360/reviews
 # local web runner deploy
 #ENDPOINT=http://localhost:8080/tcss360/reviews
 # net beans deploy
-#ENDPOINT=http://localhost:8084/sample_maven_web_app/tcss360/users
+#ENDPOINT=http://localhost:8084/sample_maven_web_app/tcss360/reviews
 # manual deploy
 #ENDPOINT=http://localhost:8080/sample_maven_web_app-1.0-SNAPSHOT/tcss360/users
 curl -X POST -H "Content-Type: application/json" -d @./new_review.json $ENDPOINT
