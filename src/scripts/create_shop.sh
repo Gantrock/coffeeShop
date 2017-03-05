7cb204b373030712b0ddd37e4d7cb43ad9156fcd
nickveilleux <{veilleux@uw.edu}>
2017-03-04 22:26:42 -0800

    test

--- src/scripts/create_shop.sh
+++ src/scripts/create_shop.sh
@@ -7,7 +7,7 @@ ENDPOINT=https://sheltered-earth-81944.herokuapp.com/tcss360/shops
 #ENDPOINT=http://localhost:8084/sample_maven_web_app/tcss360/users
 # manual deploy
 #ENDPOINT=http://localhost:8080/sample_maven_web_app-1.0-SNAPSHOT/tcss360/users
-curl -X POST -H "Content-Type: application/json" -d @./doug.json $ENDPOINT
+curl -X POST -H "Content-Type: application/json" -d @./fred.json $ENDPOINT
 #curl -X POST -H "Content-type: application/json" -d '{"name":"Fred Smith","age":33}' $ENDPOINT
 echo
 
