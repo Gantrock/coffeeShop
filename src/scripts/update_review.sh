eaf5776a4e4985f3c9edd465614f570c7c548482
nickveilleux <veilleux@uw.edu>
2017-03-07 01:15:50 -0800

    made new script

--- src/scripts/update_review.sh
+++ src/scripts/update_review.sh
@@ -1,10 +1,10 @@
-#ENDPOINT=https://sheltered-earth-81944.herokuapp.com/tcss360/reviews
+ENDPOINT=https://sheltered-earth-81944.herokuapp.com/tcss360/reviews
 # local web runner deploy
-ENDPOINT=http://localhost:8080/tcss360/reviews
+#ENDPOINT=http://localhost:8080/tcss360/reviews
 # net beans deploy
 #ENDPOINT=http://localhost:8084/sample_maven_web_app/tcss360/users
 # manual deploy
 #ENDPOINT=http://localhost:8080/sample_maven_web_app-1.0-SNAPSHOT/tcss360/users
-curl -X PUT -H "Content-Type: application/json" -d @./good_review.json $ENDPOINT
+curl -X PUT -H "Content-Type: application/json" -d @./great_review.json $ENDPOINT
 echo
 
